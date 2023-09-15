dd43dc786b67cf657b5852b6012140d894132605
bosh16 <boshk8832@gmail.com>
2023-09-15 07:46:58 -0700

    b

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Descriptian: A C program that prints with put function
+ *
+ * Return: Always 0 (sucsess)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
