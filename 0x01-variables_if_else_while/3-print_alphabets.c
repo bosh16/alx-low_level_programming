19aac94575eaa348505c98c2909164918f17b23e
bosh16 <boshk8832@gmail.com>
2023-12-29 09:11:13 -0800

    boshkash

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,18 +2,23 @@
 /**
  * main - Entry Point
  *
- * Descriptian: A C program that print the alphabet in uppercase wit putchar
+ * Descriptian: A C program that print the alphabet in lower case and uppercase
  *
  * Return: Always 0 (sucsess)
 */
 int main(void)
 {
-char ch = 'A';
-
-while (ch <= 'Z')
+char ch = 'a';
+char CH = 'A';
+while (ch <= 'z')
 {
 	putchar(ch);
 	ch++;
+}
+while (CH <= 'Z')
+{
+	putchar(CH);
+	CH++;
 }
 putchar('\n');
 	return (0);
