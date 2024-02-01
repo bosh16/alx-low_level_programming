7b0ea853d267543c0e32ff7a1b9c81ff9e3899b8
bosh16 <boshk8832@gmail.com>
2024-02-01 11:22:17 -0800

    program to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Descriptian: A C program that prints with put function
+ *
+ * Return: Always 0 (sucsess)
+*/
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
