8ee8e9dda412a86846c4b59016eff4d7b9958949
bosh16 <boshk8832@gmail.com>
2024-06-21 10:01:08 -0700

    b

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Descriptian:  print_alphabet
+ *
+ * Return: Always 0 (sucsess)
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+		for (ch = 'a' ; ch = 'z' ; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
+
